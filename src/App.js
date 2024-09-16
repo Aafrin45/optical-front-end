a05de27ef12cbf670f0eaf2802cd9c50548046a5
Anwar2606 <hfanwar26@gmail.com>
2024-09-16 07:46:21 +0530

    Feat/LoginAndNavAndProfile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/admin-dashboard/sidebar";
 import ViewProducts from "./components/shopping-page/view-products";
 function App() {
-  return (
+  return ( 
     <div>
       <Router>
         <Routes>
